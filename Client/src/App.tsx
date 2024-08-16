dc971238fae2bd5062db53ffec21dabfc89d7456
sondos-saad <so_saad@yahoo.com>
2024-08-16 18:53:37 +0300

    ProfilePage

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,11 @@
+import Profile from "./Component/profile/Profile"
 
 function App() {
 
   return (
     <>
       Hi
+      <Profile />
       <div className="bg-blue-500 text-white p-4">
         Welcome to my app!
       </div>
