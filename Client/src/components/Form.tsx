7ae330396c0231a0eb18538ed3f0a8e65c575cdb
Gehadazzam <gehadgamalazzam@gmail.com>
2024-08-17 20:04:40 +0300

    style: change the text color of delete photo btton

--- Client/src/components/Form.tsx
+++ Client/src/components/Form.tsx
@@ -60,7 +60,7 @@ function ApplicationForm() {
           </Button>
           <Button
             variant="secondary"
-            className="px-4 py-2 text-sm bg-gray-400 hover:bg-gray-500 text-white rounded-full"
+            className="px-4 py-2 text-sm bg-gray-400 hover:bg-gray-500 rounded-full"
           >
             Delete photo
           </Button>
