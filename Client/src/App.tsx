c2df47cf0577d978868ef83bedff5b82b04e79ba
1zizo1 <ahmedmohmedzizo258@gmail.com>
2024-08-18 00:26:05 +0300

    add json file to hold the prfile data

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,25 +1,16 @@
 import JobsView from './Components/JobsView/Jobsview';
 import Profile from "./components/Profile/Profile";
+import PrfileData from "./Data/Profile.json";
 
 function App() {
 
   return (
     <>
-      Hi
       <div className="flex justify-center items-center min-h-screen bg-gray-100 py-8">
       <JobsView />
       </div>
       <Profile
-      name="First last name"
-      jobTilte="Web developer"
-      profileImage="https://via.placeholder.com/150"
-      bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum."
-      country="Egypt"
-      experience="5 years in web development"
-      certificates={["Certified React Developer", "Certified JavaScript Specialist"]}
-      projects={["E-commrence cross platform ", "E-commrence cross platform ", "E-commrence cross platform "]}
-      skills={["JavaScript", "React", "TypeScript", "CSS", "HTML"]}
-      cvLink="https://example.com/john-doe-cv.pdf"
+      {...PrfileData}
     />
     </>
   )
