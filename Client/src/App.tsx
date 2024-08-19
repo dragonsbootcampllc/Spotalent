f2b8b49fca6405d8d44c9a5c18afede3c1d9e5a6
Gehadazzam <gehadgamalazzam@gmail.com>
2024-08-20 02:23:08 +0300

    feat: new Post feature

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,15 @@
+import NewPost from "./Componants/Post";
 
 function App() {
-
   return (
     <>
       Hi
-      <div className="bg-blue-500 text-white p-4">
-        Welcome to my app!
+      <div className="bg-blue-500 text-white p-4">Welcome to my app!</div>
+      <div>
+        <NewPost />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
