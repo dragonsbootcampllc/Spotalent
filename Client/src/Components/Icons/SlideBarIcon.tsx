af53f026eddade4f3b81a55694a4a422566becd2
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-08-24 22:34:05 +0300

    restructure + SVG correct formate fot the JSX

--- Client/src/Components/Icons/SlideBarIcon.tsx
+++ Client/src/Components/Icons/SlideBarIcon.tsx
@@ -2,8 +2,8 @@
 import React from 'react';
 
 const SlidBarIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
     </svg>
 
 );
--- Client/src/Components/Icons/XIcon.tsx
+++ Client/src/Components/Icons/XIcon.tsx
@@ -2,8 +2,8 @@
 
 
 const XIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
     </svg>
 
 );
