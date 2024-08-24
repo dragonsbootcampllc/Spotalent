11b1f35f2e0acd1a6bee72efd86067639c2acdf7
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-08-24 22:47:35 +0300

    [RadioGroup] input fixed

--- Client/src/Components/ApplicationQs/AppQs.tsx
+++ Client/src/Components/ApplicationQs/AppQs.tsx
@@ -1,17 +1,24 @@
 import XIcon from "../Icons/XIcon";
 import Dropdown from "./Dropdown";
-import CircleIcon from "../Icons/CircleIcon";
+import { RadioGroup } from "@lemonsqueezy/wedges";
+
 const AppQs = () => {
-    const items = Array.from({ length: 4 }, (_, index) => (
-        <div key={index} className="flex flex-row mb-2">
-            <div>
-                <CircleIcon />
+    const answers = [
+        { label: "Lorem ipsum dolor sit amet consectetur. Justo facilisis sed massa molestie volutpat purus arcu.", value: "value-1" },
+        { label: "Lorem ipsum dolor sit amet consectetur. Justo facilisis sed massa molestie volutpat purus arcu.", value: "value-2" },
+        { label: "Lorem ipsum dolor sit amet consectetur. Justo facilisis sed massa molestie volutpat purus arcu.", value: "value-3" },
+        { label: "Lorem ipsum dolor sit amet consectetur. Justo facilisis sed massa molestie volutpat purus arcu.", value: "value-4" }
+    ];
+
+    const items = answers.map((answer, index) => (
+        <div key={index} className="     mb-2">
+            <div className="mr-2">
+                <RadioGroup.Item label={answer.label} value={answer.value} />
             </div>
-            <div className="sm:text-lg font-Poppins text-sm">
-                <p>Lorem ipsum dolor sit amet consectetur. Justo facilisis sed massa molestie volutpat purus arcu.</p>
-            </div>
+           
         </div>
     ));
+
     return (
         <>
             <div className="w-full max-w-4xl mx-auto bg-white rounded-xl shadow-md shadow-[#d6d1db] mt-10 p-6">
@@ -21,7 +28,11 @@ const AppQs = () => {
                 <div>
                     <div className="flex flex-row justify-between">
                         <div className="w-full flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-gray-400 ">
-                            <input type="text" name="username" id="username" autoComplete="username"
+                            <input 
+                                type="text" 
+                                name="username" 
+                                id="username" 
+                                autoComplete="username"
                                 className="border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-[#6B7280] focus:ring-0 sm:text-sm sm:leading-6 outline-none h-10"
                                 placeholder="Question 1"
                             />
@@ -37,7 +48,12 @@ const AppQs = () => {
                         <span>+ Add Question</span>
                     </div>
                     <div className="flex flex-row-reverse">
-                        <button type="button" className=" w-full justify-center rounded-full bg-[#4B5563] px-24 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#4B5563] sm:ml-3 sm:w-auto">Post</button>
+                        <button 
+                            type="button" 
+                            className="w-full justify-center rounded-full bg-[#4B5563] px-24 py-2 text-sm font-semibold text-white shadow-sm hover:bg-[#4B5563] sm:ml-3 sm:w-auto"
+                        >
+                            Post
+                        </button>
                     </div>
                 </div>
             </div>
@@ -47,12 +63,17 @@ const AppQs = () => {
                     <span>ANSRs</span>
                 </div>
                 <div className="mb-2">
-                {items}
+                    <RadioGroup
+                        defaultValue="value-1"
+                        description="Choose an option"
+                        label="Select an Option"
+                        required
+                    >
+                        {items}
+                    </RadioGroup>
                 </div>
             </div>
         </>
-
-
     );
 };
 
