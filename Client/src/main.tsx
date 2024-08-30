5b355594558c8539010df169fdf7ea58f0e63ed3
DarkCode662432 <ff9371668@gmail.com>
2024-08-30 04:10:56 +0300

    [init] Routers

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './Pages/App.tsx'
-import './Styles/index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from './Pages/App';
+import './Styles/index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Root() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
