71383e703a5210385d22b3e4a4894b2430403db2
DarkCode662432 <ff9371668@gmail.com>
2024-10-02 19:44:27 +0300

    [enhance]

--- Client/Web/src/Pages/Layout/MainLayout.tsx
+++ Client/Web/src/Pages/Layout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../../Components/Bars/Navbar";
+import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
 
 export default function MainLayout() {
