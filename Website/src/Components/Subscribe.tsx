6b0d88c8f3a1150f34a127c6f8676a933dd7dba0
devbn3li <fudwhc@gmail.com>
2024-10-17 17:20:33 +0300

    Enhancement

--- Website/src/Components/Subscribe.tsx
+++ Website/src/Components/Subscribe.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import lolo from '../../public/Images/3d-glassy-fluid-motion-on-blue-egg-shaped-objects.png';
-import toto from '../../public/Images/Group.svg'
+import toto from '../../public/Images/Group.svg';
 
 const Subscribe: React.FC = () => {
   return (
-    <div className="relative max-md:flex-col z-0 flex items-center md:h-[300px] mx-6 justify-between bg-[#004B9C] rounded-3xl md:px-20 md:py-20 md:mx-24 md:my-20 overflow-hidden">
+    <div className="relative flex flex-col max-md:py-6 max-md:gap-10 md:flex-row z-0 items-center md:h-[300px] mx-6 px-6 justify-between bg-[#004B9C] rounded-3xl md:px-20 md:py-20 md:mx-24 md:my-20 overflow-hidden">
       <img
         src={lolo}
         alt="lolo"
@@ -12,15 +12,17 @@ const Subscribe: React.FC = () => {
         width={280}
         height={280}
       />
-      <div className='max-w-[500px] px-10 text-5xl text-white'>Never Want to Miss Any Job News?</div>
+      <div className="max-w-[500px] text-3xl md:text-5xl text-white text-center md:text-left">
+        Never Want to Miss Any Job News?
+      </div>
 
-      <div className=" bg-white gap-4 md:w-[400px] flex p-4 justify-between items-center rounded-xl overflow-hidden shadow-md">
+      <div className="bg-white gap-6 max-md:flex-col md:min-w-[400px] md:max-w-[400px] flex p-4 justify-between items-center rounded-xl overflow-hidden shadow-md">
         <input
           type="email"
           placeholder="Enter your email address here..."
-          className="px-4 text-gray-700 md:w-[250px] outline-none border-r-2 border-gray-200"
+          className="px-4 text-gray-700 w-full md:w-[250px] outline-none md:border-r-2 border-gray-200"
         />
-        <button className="bg-[#004B9C] rounded-full hover:bg-[#004B9C]/90 text-white px-4 py-1">
+        <button className="bg-[#004B9C] max-sm:w-full rounded-full hover:bg-[#004B9C]/90 text-white px-4 py-1">
           Subscribe
         </button>
       </div>
@@ -28,7 +30,7 @@ const Subscribe: React.FC = () => {
       <img
         src={toto}
         alt="toto"
-        className="md:absolute md:top-0 -right-52 h-full w-full -z-10 opacity-70"
+        className="md:absolute max-md:hidden md:top-0 md:-right-52 h-full w-full -z-10 opacity-70"
         width={300}
         height={300}
       />
