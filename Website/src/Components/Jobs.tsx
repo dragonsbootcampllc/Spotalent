1da3eb56ced7b46aa3a19a236def7d2d8470275d
devbn3li <fudwhc@gmail.com>
2024-10-21 04:52:11 +0300

    cursor-not-allowed

--- Website/src/Components/Jobs.tsx
+++ Website/src/Components/Jobs.tsx
@@ -38,8 +38,8 @@ const RecommendedJobs: React.FC<RecommendedJobsProps> = ({ jobs, selectedTags })
             <div className={`p-4 rounded-3xl ${bgColors[index % bgColors.length]}`}>
               <div className="flex justify-between items-center">
                 <span className="bg-white px-3 py-1 rounded-full text-sm">{job.datePosted}</span>
-                <div className="h-10 w-10 rounded-full bg-white flex items-center justify-center">
-                  <a href="/login" className='text-[#036BDC]'><CiBookmark size={22} /></a>
+                <div className="h-10 w-10 rounded-full cursor-not-allowed bg-white flex items-center justify-center">
+                  <a href="/login" className='text-[#036BDC] cursor-not-allowed'><CiBookmark size={22} /></a>
                 </div>
               </div>
 
@@ -70,11 +70,11 @@ const RecommendedJobs: React.FC<RecommendedJobsProps> = ({ jobs, selectedTags })
             <div className='flex flex-col sm:flex-row justify-between items-center mt-6 px-2'>
               <p className="text-lg font-semibold mb-4 sm:mb-0">{job.salary}</p>
               <div className="flex justify-between items-center gap-4">
-                <button className="bg-[#004B9C] text-white py-2 px-4 rounded-full">
-                  <a href="/login">Apply</a>
+                <button className="bg-[#004B9C] text-white py-2 px-4 rounded-full cursor-not-allowed">
+                  <a href="/login" className="cursor-not-allowed">Apply</a>
                 </button>
-                <button className="border border-[#004B9C] text-[#004B9C] py-2 px-4 rounded-full">
-                  <a href="/login">Details</a>
+                <button className="border border-[#004B9C] cursor-not-allowed text-[#004B9C] py-2 px-4 rounded-full">
+                  <a href="/login" className='cursor-not-allowed'>Details</a>
                 </button>
               </div>
             </div>
