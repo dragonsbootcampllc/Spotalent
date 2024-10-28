78fa7fbeb2d9abb69e13c91a2cab4bd91e1d4715
devbn3li <muhammed.ali.hissein@gmail.com>
2024-10-28 10:18:17 +0300

    rm components

--- Website/src/Pages/Home.tsx
+++ Website/src/Pages/Home.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Hero from '../Components/Hero';
 import Company from '../Components/Company';
 import Jobs from '../Components/Jobs';
-import Apply from '../Components/Apply';
 import CardsHover from '../Components/CardHover';
 import Grid from '../Components/Grid';
-import Comments from '../Components/Comments';
 import Pricing from '../Components/Pricing';
 import Subscribe from '../Components/Subscribe';
 import FAQSection from '../Components/FAQ';
@@ -112,10 +110,8 @@ const Home: React.FC = () => {
           "Product",
         ]}
       />
-      <Apply />
       <CardsHover />
       <Grid />
-      <Comments />
       <Pricing />
       <Subscribe />
       <FAQSection />
